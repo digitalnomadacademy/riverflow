fb004e1772d3e3e0c103cb782b93c68a3bbf9842
matej <matejthetree@gmail.com>
2022-04-25 20:46:19 +0200

    actore test

--- lib/utils/actor.dart
+++ lib/utils/actor.dart
@@ -9,11 +9,13 @@ abstract class BaseActor {
   BaseActor(this.providerContainer);
 
   @protected
+  @visibleForTesting
   T readObservable<T>(StateProvider<T> provider) {
-    return providerContainer.read(provider.notifier).state;
+    return providerContainer.read(provider);
   }
 
   @protected
+  @visibleForTesting
   void writeObservable<T>(StateProvider<T> provider, T value,
       {bool cache = false}) {
     providerContainer.read(provider.notifier).state = value;
---
+++ test/utils/actor_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverflow/utils/actor.dart';
+
+void main() {
+  test('readObservable reads correctObservable', () {
+    var providerContainer = ProviderContainer();
+    var actor = _TestActor(providerContainer);
+
+    _TestObservable observable;
+
+    //test for initial data
+    observable = actor.readObservable(_testObservableProvider);
+    expect(observable, providerContainer.read(_testObservableProvider));
+
+    // update data directly
+    providerContainer.read(_testObservableProvider.notifier).state =
+        _TestObservable(3);
+
+    // test for new data
+    observable = actor.readObservable(_testObservableProvider);
+    expect(observable, providerContainer.read(_testObservableProvider));
+  });
+
+  test('writeObservable writes correctObservable', () {
+    var providerContainer = ProviderContainer();
+    var actor = _TestActor(providerContainer);
+
+    _TestObservable observable;
+    //write initial data
+    actor.writeObservable(_testObservableProvider, _TestObservable(4));
+
+    // check if actor correctly updates the observable
+    observable = actor.readObservable(_testObservableProvider);
+    expect(observable.counter, 4);
+
+    // update data directly
+    providerContainer.read(_testObservableProvider.notifier).state =
+        _TestObservable(3);
+
+    // test for new data
+    actor.writeObservable(_testObservableProvider, _TestObservable(7));
+
+    observable = actor.readObservable(_testObservableProvider);
+    expect(observable.counter, 7);
+  });
+}
+
+class _TestActor extends BaseActor {
+  _TestActor(ProviderContainer providerContainer) : super(providerContainer);
+}
+
+var _testObservableProvider = StateProvider((red) => _TestObservable(0));
+
+class _TestObservable {
+  final int counter;
+
+  _TestObservable(this.counter);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is _TestObservable &&
+          runtimeType == other.runtimeType &&
+          counter == other.counter;
+
+  @override
+  int get hashCode => counter.hashCode;
+}
